6851966e68c2a8b9a2152c0ffe54891e764629f8
Jellyfin Adult <jellyfinadult@gmail.com>
2024-02-18 18:08:10 +0300

    fix(teamskeet): update site implementation (#29)

---
+++ Jellyfin.Plugin.Pronium.Tests/SiteAnalMomTests.cs
@@ -0,0 +1,56 @@
+using Pronium.Helpers;
+using Pronium.Helpers.Utils;
+using Pronium.Sites;
+
+namespace Pronium.UnitTests;
+
+[TestFixture]
+public class SiteAnalMomTests
+{
+    [SetUp]
+    public void Setup()
+    {
+        Database.LoadAll();
+    }
+
+    private readonly NetworkMylf _site = new();
+    private readonly string _testSceneUrl = "https://mylf.com/movies/a-hole-lot-of-help-from-my-tutor";
+
+    [Test]
+    public async Task SearchIsWorking()
+    {
+        var result = await _site.Search(new[] { 23, 26 }, "A Hole Lot of Help From My Tutor", null, new CancellationToken());
+        Assert.That(result.Count, Is.GreaterThan(10));
+        var id = result[0].ProviderIds.Values.FirstOrDefault();
+        Assert.That(id, Is.Not.Empty);
+        Assert.That(Helper.Decode(id?.Split('#')[0]), Is.EqualTo(_testSceneUrl));
+        Assert.That(result.First().ImageUrl, Does.Contain("christy_love2"));
+    }
+
+    [Test]
+    public async Task UpdateIsWorking()
+    {
+        var result = await _site.Update(new[] { 23, 26 }, new[] { Helper.Encode(_testSceneUrl), "2024-02-17" }, new CancellationToken());
+        Assert.That(result.Item.Name, Is.EqualTo("A Hole Lot of Help From My Tutor"));
+        Assert.That(result.Item.Overview, Does.StartWith("Max and Nade aren’t the sharpest tools"));
+        Assert.That(result.Item.Studios.Length, Is.EqualTo(2));
+        Assert.That(result.Item.Genres.Length, Is.EqualTo(70));
+        Assert.That(result.Item.Genres, Does.Contain("Casual Wear"));
+        Assert.That(result.People.Count, Is.EqualTo(1));
+        Assert.That(result.People.Select(t => t.Name), Does.Contain("Christy Love"));
+        Assert.That(result.Item.PremiereDate, Is.EqualTo(new DateTime(2024, 02, 17)).Within(24).Hours);
+    }
+
+    [Test]
+    public async Task GetImagesIsWorking()
+    {
+        var result = (await _site.GetImages(
+            new[] { 23, 26 },
+            new[] { Helper.Encode(_testSceneUrl), "2024-02-17" },
+            null,
+            new CancellationToken())).ToList();
+
+        Assert.That(result, Has.Count.EqualTo(2));
+        Assert.That(result.First().Url, Does.Contain("christy_love2"));
+    }
+}
---
+++ Jellyfin.Plugin.Pronium.Tests/SiteFreeUseFantasyTests.cs
@@ -0,0 +1,57 @@
+using Pronium.Helpers;
+using Pronium.Helpers.Utils;
+using Pronium.Sites;
+
+namespace Pronium.UnitTests;
+
+[TestFixture]
+public class SiteFreeUseFantasyTests
+{
+    [SetUp]
+    public void Setup()
+    {
+        Database.LoadAll();
+    }
+
+    private readonly NetworkMylf _site = new();
+    private readonly string _testSceneUrl = "https://www.teamskeet.com/movies/show-us-what-youre-good-at";
+
+    [Test]
+    public async Task SearchIsWorking()
+    {
+        var result = await _site.Search(new[] { 24, 58 }, "Show Us What You’re Good at", null, new CancellationToken());
+        Assert.That(result.Count, Is.GreaterThan(10));
+        var id = result[0].ProviderIds.Values.FirstOrDefault();
+        Assert.That(id, Is.Not.Empty);
+        Assert.That(Helper.Decode(id?.Split('#')[0]), Is.EqualTo(_testSceneUrl));
+        Assert.That(result.First().ImageUrl, Does.Contain("daisy_stone_and_dharma_jones"));
+    }
+
+    [Test]
+    public async Task UpdateIsWorking()
+    {
+        var result = await _site.Update(new[] { 24, 58 }, new[] { Helper.Encode(_testSceneUrl), "2024-02-03" }, new CancellationToken());
+        Assert.That(result.Item.Name, Is.EqualTo("Show Us What You’re Good at"));
+        Assert.That(result.Item.Overview, Does.StartWith("It’s been a while since Daisy is jobless, and her roommate"));
+        Assert.That(result.Item.Studios.Length, Is.EqualTo(2));
+        Assert.That(result.Item.Genres.Length, Is.EqualTo(83));
+        Assert.That(result.Item.Genres, Does.Contain("Barefoot"));
+        Assert.That(result.People.Count, Is.EqualTo(2));
+        Assert.That(result.People.Select(t => t.Name), Does.Contain("Daisy Stone"));
+        Assert.That(result.People.Select(t => t.Name), Does.Contain("Dharma Jones"));
+        Assert.That(result.Item.PremiereDate, Is.EqualTo(new DateTime(2024, 02, 03)).Within(24).Hours);
+    }
+
+    [Test]
+    public async Task GetImagesIsWorking()
+    {
+        var result = (await _site.GetImages(
+            new[] { 24, 58 },
+            new[] { Helper.Encode(_testSceneUrl), "2024-02-03" },
+            null,
+            new CancellationToken())).ToList();
+
+        Assert.That(result, Has.Count.EqualTo(2));
+        Assert.That(result.First().Url, Does.Contain("daisy_stone_and_dharma_jones"));
+    }
+}
