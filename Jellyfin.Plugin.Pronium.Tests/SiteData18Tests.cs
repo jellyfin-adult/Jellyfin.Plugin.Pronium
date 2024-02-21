a25d0da1de2ff019dae1e2cf713d9b4ba3103a99
Jellyfin Adult <jellyfinadult@gmail.com>
2024-02-22 00:43:47 +0300

    fix(data18): missing actors images (#35)

--- Jellyfin.Plugin.Pronium.Tests/SiteData18Tests.cs
+++ Jellyfin.Plugin.Pronium.Tests/SiteData18Tests.cs
@@ -35,6 +35,8 @@ public class SiteData18Tests
         Assert.That(result.Item.Studios.Length, Is.EqualTo(3));
         Assert.That(result.Item.Genres.Length, Is.EqualTo(6));
         Assert.That(result.People.Count, Is.EqualTo(2));
+        Assert.That(result.People[0].ImageUrl, Does.Contain("8b1888d6-4d61-4a60-9bec-d9bf3d80c785.jpg"));
+        Assert.That(result.People[1].ImageUrl, Does.Contain("7e059593-78ff-40d3-840e-1f5190791c67.jpg"));
     }
 
     [Test]
