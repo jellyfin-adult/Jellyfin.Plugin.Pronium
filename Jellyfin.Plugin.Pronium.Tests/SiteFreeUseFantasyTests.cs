52b1d3744aa3b2823e443325faf12a82bc89a79e
jellyfin-adult <jellyfinadult@gmail.com>
2024-08-10 19:43:33 +0300

    chore: improve logging (#104)

--- Jellyfin.Plugin.Pronium.Tests/SiteFreeUseFantasyTests.cs
+++ Jellyfin.Plugin.Pronium.Tests/SiteFreeUseFantasyTests.cs
@@ -14,36 +14,36 @@ public class SiteFreeUseFantasyTests
     }
 
     private readonly NetworkMylf _site = new();
-    private readonly string _testSceneUrl = "https://www.teamskeet.com/movies/show-us-what-youre-good-at";
+    private readonly string _testSceneUrl = "https://www.teamskeet.com/movies/freeused-freeloader";
 
     [Test]
     [TestCase(TestName = "{c}.{m}")]
     public async Task SearchIsWorking()
     {
-        var result = await _site.Search(new[] { 24, 58 }, "Show Us What You’re Good at", null, new CancellationToken());
-        Assert.That(result.Count, Is.GreaterThan(10));
+        var result = await _site.Search(new[] { 24, 58 }, "Freeused Freeloader", null, new CancellationToken());
+        Assert.That(result.Count, Is.GreaterThan(1));
         var id = result[0].ProviderIds.Values.FirstOrDefault();
         Assert.That(id, Is.Not.Empty);
         Assert.That(Helper.Decode(id?.Split('#')[0]), Is.EqualTo(_testSceneUrl));
-        Assert.That(result.First().ImageUrl, Does.Contain("daisy_stone_and_dharma_jones"));
+        Assert.That(result.First().ImageUrl, Does.Contain("breezy_bri_and_penelope_woods"));
     }
 
     [Test]
     [TestCase(TestName = "{c}.{m}")]
     public async Task UpdateIsWorking()
     {
-        var result = await _site.Update(new[] { 24, 58 }, new[] { Helper.Encode(_testSceneUrl), "2024-02-03" }, new CancellationToken());
-        Assert.That(result.Item.Name, Is.EqualTo("Show Us What You’re Good at"));
-        Assert.That(result.Item.OriginalTitle, Is.EqualTo("freeusefantasy - 2024-02-03 - Show Us What You’re Good at"));
-        Assert.That(result.Item.Overview, Does.StartWith("It’s been a while since Daisy is jobless, and her roommate"));
+        var result = await _site.Update(new[] { 24, 58 }, new[] { Helper.Encode(_testSceneUrl), "2024-08-10" }, new CancellationToken());
+        Assert.That(result.Item.Name, Is.EqualTo("Freeused Freeloader"));
+        Assert.That(result.Item.OriginalTitle, Is.EqualTo("freeusefantasy - 2024-08-10 - Freeused Freeloader"));
+        Assert.That(result.Item.Overview, Does.StartWith("Tyler is pissed at Penelope. She’s not contributing to rent"));
         Assert.That(result.Item.Studios.Length, Is.EqualTo(2));
-        Assert.That(result.Item.Genres.Length, Is.EqualTo(82));
-        Assert.That(result.Item.Genres, Does.Contain("Barefoot"));
+        Assert.That(result.Item.Genres.Length, Is.EqualTo(86));
+        Assert.That(result.Item.Genres, Does.Contain("Belly Button Piercings"));
         Assert.That(result.People.Count, Is.EqualTo(2));
-        Assert.That(result.People.Select(t => t.Name), Does.Contain("Daisy Stone"));
-        Assert.That(result.People.Select(t => t.Name), Does.Contain("Dharma Jones"));
+        Assert.That(result.People.Select(t => t.Name), Does.Contain("Penelope Woods"));
+        Assert.That(result.People.Select(t => t.Name), Does.Contain("Breezy Bri"));
 #pragma warning disable NUnit2021 // Incompatible types for EqualTo constraint
-        Assert.That(result.Item.PremiereDate, Is.EqualTo(new DateTime(2024, 02, 03)).Within(24).Hours);
+        Assert.That(result.Item.PremiereDate, Is.EqualTo(new DateTime(2024, 08, 10)).Within(24).Hours);
 #pragma warning restore NUnit2021 // Incompatible types for EqualTo constraint
     }
 
@@ -53,11 +53,11 @@ public class SiteFreeUseFantasyTests
     {
         var result = (await _site.GetImages(
             new[] { 24, 58 },
-            new[] { Helper.Encode(_testSceneUrl), "2024-02-03" },
+            new[] { Helper.Encode(_testSceneUrl), "2024-08-10" },
             null,
             new CancellationToken())).ToList();
 
         Assert.That(result, Has.Count.EqualTo(2));
-        Assert.That(result.First().Url, Does.Contain("daisy_stone_and_dharma_jones"));
+        Assert.That(result.First().Url, Does.Contain("breezy_bri_and_penelope_woods"));
     }
 }
