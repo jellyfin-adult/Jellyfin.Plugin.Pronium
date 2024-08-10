57ed4044c3ef66b9279d7d885215c00f5107ab1b
jellyfin-adult <jellyfinadult@gmail.com>
2024-08-10 20:33:59 +0300

    chore: disable tests (#104)

--- Jellyfin.Plugin.Pronium.Tests/SiteFreeUseFantasyTests.cs
+++ Jellyfin.Plugin.Pronium.Tests/SiteFreeUseFantasyTests.cs
@@ -1,6 +1,7 @@
 using Pronium.Helpers;
 using Pronium.Helpers.Utils;
 using Pronium.Sites;
+using Newtonsoft.Json;
 
 namespace Pronium.UnitTests;
 
@@ -18,43 +19,42 @@ public class SiteFreeUseFantasyTests
 
     [Test]
     [TestCase(TestName = "{c}.{m}")]
+    [Explicit]
     public async Task SearchIsWorking()
     {
         var result = await _site.Search(new[] { 24, 58 }, "Freeused Freeloader", null, new CancellationToken());
-        Warn.Unless(result, Is.Not.Null);
-        if (result != null) {
-            Assert.That(result.Count, Is.GreaterThan(1));
-            var id = result[0].ProviderIds.Values.FirstOrDefault();
-            Assert.That(id, Is.Not.Empty);
-            Assert.That(Helper.Decode(id?.Split('#')[0]), Is.EqualTo(_testSceneUrl));
-            Assert.That(result.First().ImageUrl, Does.Contain("breezy_bri_and_penelope_woods"));
-        }
+
+        Assert.That(result.Count, Is.GreaterThan(1));
+        var id = result[0].ProviderIds.Values.FirstOrDefault();
+        Assert.That(id, Is.Not.Empty);
+        Assert.That(Helper.Decode(id?.Split('#')[0]), Is.EqualTo(_testSceneUrl));
+        Assert.That(result.First().ImageUrl, Does.Contain("breezy_bri_and_penelope_woods"));
     }
 
     [Test]
     [TestCase(TestName = "{c}.{m}")]
+    [Explicit]
     public async Task UpdateIsWorking()
     {
         var result = await _site.Update(new[] { 24, 58 }, new[] { Helper.Encode(_testSceneUrl), "2024-08-10" }, new CancellationToken());
-        Warn.Unless(result?.Item, Is.Not.Null);
-        if (result.Item != null) {
-            Assert.That(result.Item.Name, Is.EqualTo("Freeused Freeloader"));
-            Assert.That(result.Item.OriginalTitle, Is.EqualTo("freeusefantasy - 2024-08-10 - Freeused Freeloader"));
-            Assert.That(result.Item.Overview, Does.StartWith("Tyler is pissed at Penelope. She’s not contributing to rent"));
-            Assert.That(result.Item.Studios.Length, Is.EqualTo(2));
-            Assert.That(result.Item.Genres.Length, Is.EqualTo(86));
-            Assert.That(result.Item.Genres, Does.Contain("Belly Button Piercings"));
-            Assert.That(result.People.Count, Is.EqualTo(2));
-            Assert.That(result.People.Select(t => t.Name), Does.Contain("Penelope Woods"));
-            Assert.That(result.People.Select(t => t.Name), Does.Contain("Breezy Bri"));
+
+        Assert.That(result.Item.Name, Is.EqualTo("Freeused Freeloader"));
+        Assert.That(result.Item.OriginalTitle, Is.EqualTo("freeusefantasy - 2024-08-10 - Freeused Freeloader"));
+        Assert.That(result.Item.Overview, Does.StartWith("Tyler is pissed at Penelope. She’s not contributing to rent"));
+        Assert.That(result.Item.Studios.Length, Is.EqualTo(2));
+        Assert.That(result.Item.Genres.Length, Is.EqualTo(86));
+        Assert.That(result.Item.Genres, Does.Contain("Belly Button Piercings"));
+        Assert.That(result.People.Count, Is.EqualTo(2));
+        Assert.That(result.People.Select(t => t.Name), Does.Contain("Penelope Woods"));
+        Assert.That(result.People.Select(t => t.Name), Does.Contain("Breezy Bri"));
 #pragma warning disable NUnit2021 // Incompatible types for EqualTo constraint
-            Assert.That(result.Item.PremiereDate, Is.EqualTo(new DateTime(2024, 08, 10)).Within(24).Hours);
+        Assert.That(result.Item.PremiereDate, Is.EqualTo(new DateTime(2024, 08, 10)).Within(24).Hours);
 #pragma warning restore NUnit2021 // Incompatible types for EqualTo constraint
-        }
     }
 
     [Test]
     [TestCase(TestName = "{c}.{m}")]
+    [Explicit]
     public async Task GetImagesIsWorking()
     {
         var result = (await _site.GetImages(
@@ -63,10 +63,7 @@ public class SiteFreeUseFantasyTests
             null,
             new CancellationToken())).ToList();
 
-        Warn.Unless(result, Is.Not.Null);
-        if (result != null) {
-            Assert.That(result, Has.Count.EqualTo(2));
-            Assert.That(result.FirstOrDefault()?.Url, Does.Contain("breezy_bri_and_penelope_woods"));
-        }
+        Assert.That(result, Has.Count.EqualTo(2));
+        Assert.That(result.FirstOrDefault()?.Url, Does.Contain("breezy_bri_and_penelope_woods"));
     }
 }
